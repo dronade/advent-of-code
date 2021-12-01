4043826149405649c9e4e6b0df5b5759754efff1
Emily Canto <56757680+dronade@users.noreply.github.com>
2021-12-01 21:32:23 +0000

    day1 part2

--- 2021/day1/day1.go
+++ 2021/day1/day1.go
@@ -10,36 +10,49 @@ import (
 func readInput(path string) ([]int, error) {
 	file, err := os.Open(path)
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
 	defer file.Close()
-  
 	var text []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-	  text = append(text, scanner.Text())
+		text = append(text, scanner.Text())
 	}
 	ints := make([]int, len(text))
 	for i, s := range text {
-	  ints[i], _ = strconv.Atoi(s)
+		ints[i], _ = strconv.Atoi(s)
 	}
 	return ints, scanner.Err()
-  }
-  
-  func main() {
+}
+
+func main() {
 	ints, err := readInput("input.txt")
 	if err != nil {
-	  log.Fatalf("readLines: %s", err)
+		log.Fatalf("readLines: %s", err)
 	}
 	increase := 0
-	
-	for i, output := range ints {
-	  if i == 0 {
-		continue
-	  }
-	  if output > ints[i - 1] {
-		increase ++
-	  }
+	// part 1:
+	// for i, output := range ints {
+	//   if i == 0 {
+	// 	continue
+	//   }
+	//   if output > ints[i - 1] {
+	// 	increase ++
+	//   }
+	// }
+
+	//part 2
+	var sums []int
+	for i := 3; i <= len(ints); i++ {
+		sums = append(sums, ints[i-1]+ints[i-2]+ints[i-3])
+	}
+	for i, output := range sums {
+		if i == 0 {
+			continue
+		}
+		if output > sums[i-1] {
+			increase++
+		}
 	}
 	print(increase)
-  }
+}
